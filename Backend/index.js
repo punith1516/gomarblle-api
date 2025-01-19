1177bc140c193114340b2e6af1e02a24d97c7007
Punith <122201042+punith1516@users.noreply.github.com>
2025-01-19 22:23:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const extractRoutes = require("./routes/extractRoutes");
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use("/api", extractRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
